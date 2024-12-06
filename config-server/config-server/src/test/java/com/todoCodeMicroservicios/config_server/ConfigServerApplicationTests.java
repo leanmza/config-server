809e23fe26a580ca4f8725bfbdb05f7fc20914bd
Leandro Hidalgo <boshahidalgo@hotmail.com>
2024-12-06 15:55:43 -0300

    primer commit

---
+++ config-server/config-server/src/test/java/com/todoCodeMicroservicios/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoCodeMicroservicios.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
